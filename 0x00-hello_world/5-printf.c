775b735a1710c979bead8b1984e440483ae522ff
chibuike777 <bountyonyejegbu0@gmail.com>
2022-10-19 14:01:07 -0700

    fix

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,9 +4,9 @@
  *
  * return: always 0 (success)
  */
-int main(void)
+int main()
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art, ");
 
-	return 0;
+	return (0);
 }
