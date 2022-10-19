cb8f52e475ac5858d4e76733ef67956aa80c57d3
chibuike777 <bountyonyejegbu0@gmail.com>
2022-10-19 15:37:09 -0700

    fix

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 /**
- * main - entry point
+ * main - prints with proper grammar, but the outcome is a piece of art,
  *
  * return: always 0 (success)
  */
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 /**
- * main - entry point
+ * main - prints size of char
  *
  * return: always 0 (success)
  */
