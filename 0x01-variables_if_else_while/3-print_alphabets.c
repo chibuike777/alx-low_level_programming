27f20524ad9b5da0efffd174cc89f1cd6d594aa7
chibuike777 <bountyonyejegbu0@gmail.com>
2022-10-22 12:00:24 -0700

    done

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+/**
+ * main - print alphabet caps and small
+ *
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	int n, m;
+
+	n = 'a';
+	m = 'A';
+
+	while (n <= 'z')
+	{
+		putchar('n');
+		n++;
+	}
+	while (n <= 'Z')
+	{
+		putchar('m');
+		m++;
+	}
+	putchar('\n');
+	return (0);
+}
