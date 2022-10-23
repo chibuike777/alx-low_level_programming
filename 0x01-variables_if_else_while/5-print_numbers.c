dccf9698f6afc3242fa096342d129143accb0586
chibuike777 <bountyonyejegbu0@gmail.com>
2022-10-23 13:51:30 -0700

    fix

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+/**
+ * main - print 0-9
+ *
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	int n;
+
+	n = 0;
+	while (n < 10)
+	{
+		putchar(n);
+		putchar('\n');
+		n++;
+	}
+	return (0);
+}
