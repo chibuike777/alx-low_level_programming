27d52975028efddfbfb573ea86ed02098a36fedf
chibuike777 <bountyonyejegbu0@gmail.com>
2022-10-26 17:59:55 -0700

    done

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,32 +6,23 @@
  */
 int main(void)
 {
-	int m, n, o, p; /*initialize 4 variables*/
+	int m, n; /*initialize 2 variables*/
 
 	for (m = 0; m < 10; m++) /*loop to print first number*/
 	{
 		for (n = 0; n < 10; n++) /*loop to print second number*/
 		{
-			for (o = 0; o < 10; o++) /*loop to print third number*/
+			if (m < n)
 			{
-				for (p = 0; p < 10; p++) /*loop to print fourth number*/
+				putchar(m + '0');
+				putchar(n + '0');
+				putchar(32);
+				putchar(o + '0');
+				putchar(p + '0');
+				if (!((m == 98) || (n == 99)))
 				{
-					if ((m <= o) && (n <= p)) /*avoid repetition*/
-					{
-						if (!((m == o) && (n == p)))
-						{	
-							putchar(m + '0');
-							putchar(n + '0');
-							putchar(32);
-							putchar(o + '0');
-							putchar(p + '0');
-							if (!((m == 9) && (n == 8) && (o == 9) && (p == 9))) 
-							{
-								putchar(44);
-								putchar(32);
-							}
-						}
-					}
+					putchar(44);
+					putchar(32);
 				}
 			}
 		}
